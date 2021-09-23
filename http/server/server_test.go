5814e9f49d91c1f1c34bb2a1ed8f9859af33be70
inabajunmr <inabajun.for.regi@gmail.com>
2021-09-23 13:21:51 +0900

    refactor and e2e test

---
+++ http/server/server_test.go
@@ -0,0 +1,119 @@
+package server
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	go Serve()
+	m.Run()
+	Stop()
+}
+
+func TestGet(t *testing.T) {
+	resp, err := http.Get("http://localhost:80")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertResponse(t, b, "", "GET", "/", "HTTP/1.1",
+		"USER-AGENT: Go-http-client/1.1", "HOST: localhost:80", "ACCEPT-ENCODING: gzip")
+}
+
+func TestPost(t *testing.T) {
+	resp, err := http.Post("http://localhost:80",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("aaaaabbbbbccccc"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertResponse(t, b, "aaaaabbbbbccccc", "POST", "/", "HTTP/1.1",
+		"CONTENT-TYPE: application/x-www-form-urlencoded", "USER-AGENT: Go-http-client/1.1",
+		"CONTENT-LENGTH: 15", "HOST: localhost:80", "ACCEPT-ENCODING: gzip")
+}
+
+func TestPost_Chunkded(t *testing.T) {
+	rd, wr := io.Pipe()
+
+	req, err := http.NewRequest("POST", "http://localhost:80", rd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		wr.Write([]byte("hello"))
+		wr.Write([]byte("hello"))
+		wr.Write([]byte("hello"))
+		wr.Close()
+	}()
+	req.TransferEncoding = []string{"chunked"}
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	b, _ := ioutil.ReadAll(resp.Body)
+
+	assertResponse(t, b, "hellohellohello", "POST", "/", "HTTP/1.1",
+		"USER-AGENT: Go-http-client/1.1",
+		"HOST: localhost:80", "ACCEPT-ENCODING: gzip",
+		"TRANSFER-ENCODING: chunked")
+}
+
+func assertResponse(t *testing.T, response []byte, expectedBody string, expectedMethod string, expectedRequestTarget string, expectedVersion string, expectedHeaders ...string) {
+	res := map[string]interface{}{}
+	json.Unmarshal(response, &res)
+
+	if res["body"] != expectedBody {
+		t.Errorf("Unexpected body:%v.", res["body"])
+	}
+	assertHeaders(t, res["headers"].([]interface{}), expectedHeaders...,
+	)
+	if res["method"] != expectedMethod {
+		t.Errorf("Unexpected method:%v.", res["method"])
+	}
+	if res["request_target"] != expectedRequestTarget {
+		t.Errorf("Unexpected request_target:%v.", res["request_target"])
+	}
+	if res["version"] != expectedVersion {
+		t.Errorf("Unexpected version:%v.", res["version"])
+	}
+}
+
+func assertHeaders(t *testing.T, actual []interface{}, expected ...string) {
+	if len(expected) != len(actual) {
+		t.Errorf("Unexpected header:%v.", actual)
+	}
+	for _, e := range expected {
+		if !containsHeader(e, actual) {
+			t.Errorf("Unexpected header:%v. expected:%v.", actual, e)
+		}
+	}
+
+}
+
+func containsHeader(header string, headers []interface{}) bool {
+	for _, h := range headers {
+		if h == header {
+			return true
+		}
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -1,59 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"net"
-	"os"
-
-	"github.com/inabajunmr/http11server/http"
-	"github.com/inabajunmr/http11server/http/request"
-	"github.com/inabajunmr/http11server/http/response"
-)
+import "github.com/inabajunmr/http11server/http/server"
 
 func main() {
-	service := ":80"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	checkError(err)
-	listener, err := net.ListenTCP("tcp4", tcpAddr)
-	checkError(err)
-	for {
-		log.Println("Listen")
-		conn, err := listener.Accept()
-		if err != nil {
-			return
-		}
-
-		reader := bufio.NewReader(conn)
-		req, err := request.ParseRequest(reader)
-		if err != nil {
-			switch httpErr := err.(type) {
-			case *http.HTTPError:
-				res := &response.EchoResponse{Version: http.HTTP11, StatusCode: httpErr.Status, ReasonPhrase: httpErr.Msg}
-				res.Response(conn)
-				conn.Close()
-			default:
-				res := &response.EchoResponse{Version: http.HTTP11, StatusCode: 503, ReasonPhrase: "Service Unavailable"}
-				res.Response(conn)
-				conn.Close()
-			}
-			continue
-		}
-
-		log.Println(req.StartLine.ToString())
-		log.Println(req.Headers.ToString())
-		log.Println(string(req.Body))
-
-		res := &response.EchoResponse{Version: http.HTTP11, StatusCode: 200, ReasonPhrase: "OK", Request: *req}
-		res.Response(conn)
-		conn.Close()
-	}
-}
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
-		os.Exit(1)
-	}
+	server.Serve()
 }
